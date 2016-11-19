fd6ae9467c603846b848dee88f5b557435f42321
fontaine pierre <pierre.ftn64@gmail.com>
2016-11-19 18:05:50 +0100

    good start

---
+++ src/js/path.js
@@ -0,0 +1,39 @@
+function createPageFromTemplate(template) {
+  var container;
+  var toInsert;
+
+  console.log(template);
+
+  container = document.getElementById('container');
+  toInsert = template.getElementById('main');
+  container.appendChild(toInsert);
+}
+
+function loadTemplate(name){
+  var req = new XMLHttpRequest();
+  console.log(name);
+  req.open("GET","./template/"+name.slice(1,name.length)+".html",true);
+  req.responseType = 'document';
+  req.overrideMimeType('text/html');
+  req.onerror = function () {
+    console.log("erreur de chargement de " + name);
+  };
+
+  req.onload = function(){
+    if(req.status === 200) {
+      var template = req.responseXML;
+      console.log(template);
+      createPageFromTemplate(template);
+    } else {
+      console.log("Error"+ req.status);
+    }
+  };
+  req.send();
+}
+
+window.onload = function () {
+  if (window.location.hash === '') {
+    window.location.hash = "home";
+  }
+  loadTemplate(window.location.hash);
+};
