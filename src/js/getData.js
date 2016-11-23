a924bfc51c6445052d751ba7d35cc8cd3b5effb8
fontaine pierre <pierre.ftn64@gmail.com>
2016-11-23 10:02:20 +0100

    modulaire de ouf

--- src/js/getData.js
+++ src/js/getData.js
@@ -91,16 +91,28 @@ function dataEmission () {
 
       tabItem = response.getElementsByTagName("item");
       for (var i = 0; i < tabItem.length; i++) {
-        var tbody,tr,td,td_2;
-        console.log(tabItem[i].childNodes[1].innerHTML);
+        var tbody,tr,td,td_2,td_3,audio;
+
+        //console.log(tabItem[i].childNodes[1].innerHTML);
+        //console.log(tabItem[i].childNodes[11].getAttribute('url'));
+
+        audio = document.createElement('AUDIO');
+        audio.src = tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('url');
+        audio.controls="controls";
+        audio.preload = "false";
         tr = document.createElement('tr');
         td = document.createElement('td');
         td_2 = document.createElement('td');
+        td_3 = document.createElement('td');
         tbody = document.createElement('tbody');
         td_2.innerHTML = i;
-        td.innerHTML = tabItem[i].childNodes[1].innerHTML;
+        td.innerHTML = tabItem[i].getElementsByTagName('title')[0].innerHTML;
+        td_3.appendChild(audio);
         tr.appendChild(td_2);
         tr.appendChild(td);
+        tr.appendChild(td_3);
+        console.log(audio);
+
         tbody.appendChild(tr);
         tableauHTML.appendChild(tbody);
       }
