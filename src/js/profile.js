ad0c405436afccfb4ad0a45fd3dde6998a3689be
fontaine pierre <pierre.ftn64@gmail.com>
2016-11-24 10:41:27 +0100

    gestion des contenus

--- src/js/profile.js
+++ src/js/profile.js
@@ -13,16 +13,24 @@ function userRss () {
           var tableauHTML = document.getElementById("tableau");
           tabItem = response.getElementsByTagName("item");
           for (var i = 0; i < tabItem.length; i++) {
-            var tbody,tr,td,td_2,td_3,audio;
+            var tbody,tr,td,td_2,td_3,media,type;
 
             //console.log(tabItem[i].childNodes[1].innerHTML);
             //console.log(tabItem[i].childNodes[11].getAttribute('url'));
-
-            audio = document.createElement('AUDIO');
-            audio.src = tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('url');
             console.log(tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('type'));
-            audio.controls="controls";
-            audio.preload = "false";
+            type = tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('type');
+            if (type == "audio/mpeg") {
+              console.log("met de l'audio gros");
+              media = document.createElement('AUDIO');
+              media.controls="controls";
+              media.preload = "false";
+            } else if (type == "image/jpg" || type == "image/jpeg"){
+              media = document.createElement('img');
+              media.width = 100;
+              media.height = 100;
+
+            }
+            media.src = tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('url');
             tr = document.createElement('tr');
             td = document.createElement('td');
             td_2 = document.createElement('td');
@@ -30,11 +38,11 @@ function userRss () {
             tbody = document.createElement('tbody');
             td_2.innerHTML = i;
             td.innerHTML = tabItem[i].getElementsByTagName('title')[0].innerHTML;
-            td_3.appendChild(audio);
+            td_3.appendChild(media);
             tr.appendChild(td_2);
             tr.appendChild(td);
             tr.appendChild(td_3);
-            console.log(audio);
+            console.log(media);
 
             tbody.appendChild(tr);
             tableauHTML.appendChild(tbody);
