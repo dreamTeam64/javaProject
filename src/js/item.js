69a8fb38ba8325a3edff79fc028bef579f34181a
fontaine pierre <pierre.ftn64@gmail.com>
2016-11-26 16:35:56 +0100

    plus de modularité

---
+++ src/js/item.js
@@ -0,0 +1,17 @@
+var Item = {
+  init: function(item){
+    this.item = item;
+  },
+  getTitle: function(){
+    return (this.item).getElementsByTagName('title')[0].innerHTML;
+  },
+  getEnclosureType: function(){
+    return (this.item).getElementsByTagName('enclosure')[0].getAttribute('type');
+  },
+  getEnclosureContent: function(){
+    return (this.item).getElementsByTagName('enclosure')[0].getAttribute('url');
+  },
+  getPublicationDate: function(){
+    return (this.item).getElementsByTagName('pubDate')[0].innerHTML;
+  }
+};
--- src/js/profile.js
+++ src/js/profile.js
@@ -7,50 +7,48 @@ function userRss () {
     console.log(button);
 
     button.addEventListener("click",function(){
-        console.log("wesh");
-        getXML(link.value).then(function(response){
-          var tabItem;
-          var tableauHTML = document.getElementById("tableau");
-          tabItem = response.getElementsByTagName("item");
-          for (var i = 0; i < tabItem.length; i++) {
-            var tbody,tr,td,td_2,td_3,media,type;
+      var flux;
+      var tabItem;
+      var tableauHTML = document.getElementById("tableau");
 
-            //console.log(tabItem[i].childNodes[1].innerHTML);
-            //console.log(tabItem[i].childNodes[11].getAttribute('url'));
-            console.log(tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('type'));
-            type = tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('type');
-            if (type == "audio/mpeg") {
-              console.log("met de l'audio gros");
-              media = document.createElement('AUDIO');
-              media.controls="controls";
-              media.preload = "false";
-            } else if (type == "image/jpg" || type == "image/jpeg"){
-              media = document.createElement('img');
-              media.width = 100;
-              media.height = 100;
+      flux = Object.create(rss);
+      console.log(flux);
+      flux.init(link.value);
 
-            }
-            media.src = tabItem[i].getElementsByTagName('enclosure')[0].getAttribute('url');
-            tr = document.createElement('tr');
-            td = document.createElement('td');
-            td_2 = document.createElement('td');
-            td_3 = document.createElement('td');
-            tbody = document.createElement('tbody');
-            td_2.innerHTML = (tabItem[i].getElementsByTagName('pubDate')[0]).innerHTML;
-            td.innerHTML = tabItem[i].getElementsByTagName('title')[0].innerHTML;
-            td_3.appendChild(media);
-            tr.appendChild(td_2);
-            tr.appendChild(td);
-            tr.appendChild(td_3);
-            console.log(media);
-
-            tbody.appendChild(tr);
-            tableauHTML.appendChild(tbody);
+      flux.getItems(function(tabItem){
+        console.log(tabItem);
+        for (var i = 0; i < tabItem.length; i++) {
+          var tbody,tr,td,td_2,td_3;
+          var item = Object.create(Item);
+          item.init(tabItem[i]);
+          if (item.getEnclosureType() == "audio/mpeg") {
+            media = document.createElement('AUDIO');
+            media.controls="controls";
+            media.preload = "false";
+          } else if (item.getEnclosureType() == "image/jpg" || item.getEnclosureType() == "image/jpeg"){
+            media = document.createElement('img');
+            media.width = 100;
+            media.height = 100;
           }
-          console.log(tabItem);
-        },function(Error){
+          media.src = item.getEnclosureContent();
+          tr = document.createElement('tr');
+          td = document.createElement('td');
+          td_2 = document.createElement('td');
+          td_3 = document.createElement('td');
+          tbody = document.createElement('tbody');
+          td_2.innerHTML = item.getPublicationDate();
+          td.innerHTML = item.getTitle();
+          td_3.appendChild(media);
+          tr.appendChild(td_2);
+          tr.appendChild(td);
+          tr.appendChild(td_3);
+          console.log(media);
 
-        });
+          tbody.appendChild(tr);
+          tableauHTML.appendChild(tbody);
+        }
+        console.log(tabItem);
+      });
     });
   }
 }
